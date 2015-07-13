0920c53f6511472710f2441e8d6591eccc6a86f4
lanou3g <564276728@qq.com>
2015-07-13 15:06:24 +0800

    New Code<7.13.06>

---
+++ 2222222/2222222/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2222222
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 my.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2222222/2222222/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2222222
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 my.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
